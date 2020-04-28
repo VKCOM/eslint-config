5e8e2314b88f36617215294e3e6187ca89680610
Igor Fedorov <iamfedoroff@yandex.ru>
2020-04-28 16:44:06 +0300

    Use singlequotes in JS files

--- react.js
+++ react.js
@@ -1,25 +1,25 @@
 module.exports = {
-  "parser": "babel-eslint",
-  "extends": [
-    "./index.js"
+  parser: 'babel-eslint',
+  extends: [
+    './index.js',
   ],
-  "plugins": [
-    "react",
-    "react-hooks"
+  plugins: [
+    'react',
+    'react-hooks',
   ],
-  "rules": {
-    "react-hooks/rules-of-hooks": "error",
-    "react/jsx-uses-react": 2,
-    "react/jsx-uses-vars": 2,
-    "react/react-in-jsx-scope": 2,
-    "react/prop-types": 2,
-    "react/jsx-indent": ["error", 2],
-    "react/jsx-indent-props": ["error", 2],
-    "react/jsx-curly-brace-presence": "error"
+  rules: {
+    'react-hooks/rules-of-hooks': 'error',
+    'react/jsx-uses-react': 2,
+    'react/jsx-uses-vars': 2,
+    'react/react-in-jsx-scope': 2,
+    'react/prop-types': 2,
+    'react/jsx-indent': ['error', 2],
+    'react/jsx-indent-props': ['error', 2],
+    'react/jsx-curly-brace-presence': 'error',
   },
-  "settings": {
-    "react": {
-      "version": "detect"  // Tells eslint-plugin-react to automatically detect the version of React to use
-    }
-  }
-}
+  settings: {
+    react: {
+      version: 'detect', // Tells eslint-plugin-react to automatically detect the version of React to use
+    },
+  },
+};
