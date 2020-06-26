a0fd631cc908334845c0cff2284489c4b585c855
Igor Fedorov <iamfedoroff@yandex.ru>
2020-06-26 14:13:14 +0300

    Fix React TS config rules erasing

--- typescript/react.js
+++ typescript/react.js
@@ -1,9 +1,12 @@
 module.exports = {
   parser: '@typescript-eslint/parser', // Specifies the ESLint parser
   extends: [
-    './index.js',
     '../react.js',
     'plugin:react/recommended', // Uses the recommended rules from @eslint-plugin-react
+
+    // Include the base TS config at the very end,
+    // because the rules from ../react.js extend the base config for JS and erase some rules
+    './index.js',
   ],
   rules: {
     'react/prop-types': 'off',
